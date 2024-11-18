98b848b149725184c7ff707bd7c0939a6523bc3f
Ghaith Alkhudhiri <ghaith.alkhudiri@tamarran.com>
2024-11-18 16:38:55 +0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+import { Text, View } from 'react-native';
+
+function App() {
+  return (
+    <View style={{flex: 1, backgroundColor: 'blue', alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 20, color: 'white'}}>Hello From create react app + react native web + react-native-app</Text>
+    </View>
+  );
+}
+
+export default App;
