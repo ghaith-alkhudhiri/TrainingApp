c3757701b7581dea354e8f2e552e9f6a529e8236
Sara-Husain <sara.husain@tamarran.com>
2024-12-29 10:58:00 +0300

    Shop page and components

--- src/Assets/Icons/RightArrow.tsx
+++ src/Assets/Icons/RightArrow.tsx
@@ -9,8 +9,8 @@ export class RightArrow extends Component<Props> {
     const { color='#0165FC' } = this.props;
     return (
         <Svg width="20" height="21" viewBox="0 0 20 21" fill="none">
-            <Path d="M12.0249 15.5581L17.0833 10.4998L12.0249 5.44144" stroke={color} strokeWidth="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-            <Path d="M2.91665 10.5L16.9417 10.5" stroke={color} strokeWidth="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            <Path d="M12.0249 15.5581L17.0833 10.4998L12.0249 5.44144" stroke={color} strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <Path d="M2.91665 10.5L16.9417 10.5" stroke={color} strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
         </Svg>
     )
   }
